b29d0c6df06aef62cc60b0a059c0e4212586d3de
Dima Boiko <boikod887@gmail.com>
2023-07-03 19:44:38 +0300

    api update

--- app/config/api.js
+++ app/config/api.js
@@ -1,8 +1,8 @@
 export const url = 'https://no-name.io/'
 // export const url = 'http://localhost:5000/'
 
-// const uri = 'https://noname-backend-production.up.railway.app'
-const uri = 'http://localhost:5000'
+const uri = 'https://noname-backend-production.up.railway.app'
+// const uri = 'http://localhost:5000'
 
 export const config = {
     createUrl: (path) => `${uri}/api/${path}`
