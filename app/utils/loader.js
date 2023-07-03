e2130d03307019a6dbecd0f94e20bb03a286561f
Dima Boiko <boikod887@gmail.com>
2023-07-03 19:47:20 +0300

    loader update

--- app/utils/loader.js
+++ app/utils/loader.js
@@ -1,5 +1,5 @@
-// const API = 'https://noname-backend-production.up.railway.app/api/static'
-const API = 'http://localhost:5000/api/static'
+const API = 'https://noname-backend-production.up.railway.app/api/static'
+// const API = 'http://localhost:5000/api/static'
 export default (src) => {
     return `${API}${src}`
 }
